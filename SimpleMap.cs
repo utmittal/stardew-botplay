478362a8b379f4a1c05e2bceb6b4ddd112b85587
Utkarsh Mittal <mittal.utk@gmail.com>
2024-04-03 01:42:35 +1100

    Started Pathfinding but I don't like it.

--- SimpleMap.cs
+++ SimpleMap.cs
@@ -13,6 +13,7 @@ namespace BotPlay {
 
         private readonly Map Map;
 
+        // I don't think this should actually be shared because the underlying map could change
         private bool[,]? WalkableTiles = null;
         public SimpleMap(Map map) {
             this.Map = map;
@@ -66,11 +67,65 @@ namespace BotPlay {
             }
         }
 
+        public void FindPath((int x, int y) origin, (int x, int y) destination) {
+            GenerateWalkableTiles();
+
+            // WalkableTiles is not null here but it points to a problem in the class.
+            // Weights represents the presence of a path between tiles. 0 means no path.
+            // Since it's an int array, everything should be 0 by default.
+            // This 4d matrix is dumb
+            int[,,,] weights = new int[WalkableTiles.GetLength(0), WalkableTiles.GetLength(1), WalkableTiles.GetLength(0), WalkableTiles.GetLength(1)];
+
+            for (int i = 0; i < WalkableTiles.GetLength(0); i++) {
+                for (int j = 0; j < WalkableTiles.GetLength(1); j++) {
+                    // up
+                    if (i != 0) {
+                        int upI = i - 1;
+                        int upJ = j;
+
+                        if (WalkableTiles[upI, upJ] == true) {
+                            weights[i, j, upI, upJ] = 1;
+                        }
+                    }
+                    // right
+                    if (j != WalkableTiles.GetLength(1)-1) {
+                        int rightI = i;
+                        int rightJ = j+1;
+
+                        if (WalkableTiles[rightI, rightJ] == true) {
+                            weights[i, j, rightI, rightJ] = 1;
+                        }
+                    }
+                    // down
+                    if (i != WalkableTiles.GetLength(0) - 1) {
+                        int downI = i+1;
+                        int downj = j;
+
+                        if (WalkableTiles[downI, downj] == true) {
+                            weights[i, j, downI, downj] = 1;
+                        }
+                    }
+                    // left
+                    if (j != 0) {
+                        int leftI = i;
+                        int leftJ = j -1;
+
+                        if (WalkableTiles[leftI, leftJ] == true) {
+                            weights[i, j, leftI, leftJ] = 1;
+                        }
+                    }
+                }
+            }
+
+            // BFS
+            // This is kinda stupid. You now have to do up down left right all over again. Or I guess you can just search "row" (i,j) to find weight = 1 and those are your connected vertices.
+            // But really, this should be cleaner.
+        }
 
         /// <summary>
         /// Generates a boolean tile matrix representing the map where 'true' means walkable and 'false' means blocked/water tile.
         /// </summary>
-        public void GenerateWalkableTiles() {
+        private void GenerateWalkableTiles() {
             int height = GetHeight();
             int width = GetWidth();
 
