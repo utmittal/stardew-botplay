d71133b55841c768c0eb57f85d72c841dbab4b9c
Utkarsh Mittal <mittal.utk@gmail.com>
2024-04-02 18:49:33 +1100

    Add mod entry point for bot player.
    * Includes an input simulator class that can be used with the game code.
    * Includes basic path finding from bed to door.

---
+++ InputSimulator.cs
@@ -0,0 +1,71 @@
+﻿using StardewValley.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotPlay {
+    internal class InputSimulator : IInputSimulator {
+
+        public InputSimulator() {
+        }
+
+        public bool ActionButtonPressed { get; set; } = false;
+        public bool SwitchToolButtonPressed { get; set; } = false;
+        public bool UseToolButtonPressed { get; set; } = false;
+        public bool UseToolButtonReleased { get; set; } = false;
+        public bool AddItemToInventoryButtonPressed { get; set; } = false;
+        public bool CancelButtonPressed { get; set; } = false;
+        public bool MoveUpPressed { get; set; } = false;
+        public bool MoveRightPressed { get; set; } = false;
+        public bool MoveLeftPressed { get; set; } = false;
+        public bool MoveDownPressed { get; set; } = false;
+        public bool MoveUpReleased { get; set; } = false;
+        public bool MoveRightReleased { get; set; } = false;
+        public bool MoveLeftReleased { get; set; } = false;
+        public bool MoveDownReleased { get; set; } = false;
+        public bool MoveDownHeld { get; set; } = false;
+        public bool MoveUpHeld { get; set; } = false;
+        public bool MoveRightHeld { get; set; } = false;
+        public bool MoveLeftHeld { get; set; } = false;
+        public void SimulateInput(
+            ref bool actionButtonPressed, 
+            ref bool switchToolButtonPressed, 
+            ref bool useToolButtonPressed, 
+            ref bool useToolButtonReleased, 
+            ref bool addItemToInventoryButtonPressed, 
+            ref bool cancelButtonPressed, 
+            ref bool moveUpPressed, 
+            ref bool moveRightPressed, 
+            ref bool moveLeftPressed, 
+            ref bool moveDownPressed, 
+            ref bool moveUpReleased, 
+            ref bool moveRightReleased, 
+            ref bool moveLeftReleased, 
+            ref bool moveDownReleased,
+            ref bool moveUpHeld,
+            ref bool moveRightHeld,
+            ref bool moveLeftHeld,
+            ref bool moveDownHeld) {
+            actionButtonPressed = ActionButtonPressed;
+            switchToolButtonPressed = SwitchToolButtonPressed;
+            useToolButtonPressed = UseToolButtonPressed;
+            useToolButtonReleased = UseToolButtonReleased;
+            addItemToInventoryButtonPressed = AddItemToInventoryButtonPressed;
+            cancelButtonPressed = CancelButtonPressed;
+            moveUpPressed = MoveUpPressed;
+            moveRightPressed = MoveRightPressed;
+            moveLeftPressed = MoveLeftPressed;
+            moveDownPressed = MoveDownPressed;
+            moveUpReleased = MoveUpReleased;
+            moveRightReleased = MoveRightReleased;
+            moveLeftReleased = MoveLeftReleased;
+            moveDownReleased = MoveDownReleased;
+            moveUpHeld = MoveUpHeld;
+            moveRightHeld = MoveRightHeld;
+            moveLeftHeld = MoveLeftHeld;
+            moveDownHeld = MoveDownHeld;
+        }
+    }
+}
---
+++ ModEntry.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Characters;
+using StardewValley.Util;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BotPlay {
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod {
+        private bool playing = false;
+
+        IModHelper? helper;
+
+        private InputSimulator inputSimulator = new InputSimulator();
+
+        int targetX = 3;
+        int targetY = 12;
+        bool goToTarget = false;
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper) {
+            this.helper = helper;
+
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+
+            helper.Events.GameLoop.UpdateTicked += GameLoop_UpdateTicked;
+        }
+
+        private void GameLoop_UpdateTicked(object? sender, UpdateTickedEventArgs e) {
+            if (goToTarget == false || !Context.IsWorldReady) {
+                return;
+            }
+
+            Log($"current location coordinate: {Game1.player.Tile.X},{Game1.player.Tile.Y}");
+
+            if (Game1.player.Tile.X != targetX) {
+                inputSimulator.MoveLeftHeld = true;
+            }
+            else if(Game1.player.Tile.X == targetX) {
+                inputSimulator.MoveLeftHeld = false;
+            }
+
+            if (Game1.player.Tile.Y != targetY) {
+                inputSimulator.MoveDownHeld = true;
+            }
+            else if (Game1.player.Tile.Y == targetY) {
+                inputSimulator.MoveDownHeld = false;
+            }
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e) {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            // print button presses to the console window
+            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+
+            if (e.Button == SButton.OemTilde) {
+                if (playing == false) {
+                    playing = true;
+                    Log("Started playing.");
+                    InitInputSimulator();
+                    Play();
+                }
+                else if (playing == true) {
+                    playing = false;
+                    cleanupInputSimulator();
+                    Log("Stopped playing.");
+                    return;
+                }
+            }
+        }
+
+        private void Play() {
+            Log($"current location name {Game1.currentLocation.Name}");
+            Log($"current location coordinate: {Game1.player.Tile.X},{Game1.player.Tile.Y}");
+            Log("warp locations:\n");
+            foreach (Warp warp in Game1.currentLocation.warps) {
+                Log($"\t{warp.X},{warp.Y}: {warp.TargetName}");
+            }
+
+            goToTarget = true;
+         }
+
+        private void InitInputSimulator() {
+            IReflectedField<IInputSimulator>? reflectedInputSimulator = 
+                helper?.Reflection.GetField<IInputSimulator>(typeof(Game1), "inputSimulator", true);
+            if (reflectedInputSimulator?.GetValue() == null) {
+                Log("Initializing reflected input simulator.");
+                reflectedInputSimulator?.SetValue(inputSimulator);
+            }
+        }
+
+        private void cleanupInputSimulator() {
+            IReflectedField<IInputSimulator>? reflectedInputSimulator =
+                helper?.Reflection.GetField<IInputSimulator>(typeof(Game1), "inputSimulator", true);
+            if (reflectedInputSimulator?.GetValue() != null) {
+                Log("Deinitializing reflected input simulator.");
+                reflectedInputSimulator?.FieldInfo.SetValue(reflectedInputSimulator.GetValue(), null);
+            }
+        }
+
+        private void Log(string message) {
+            this.Monitor.Log(message, LogLevel.Debug);
+        }
+    }
+}
